205f7abd6e03d5217041fccf2b663ac470d7c2a4
Divyanshi Singh Bhadauriya <singhbhadauriyadivyanshi@gmail.com>
2025-05-10 19:51:56 +0530

    Initial commit

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ app.get('/sayHello', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get('/sayHello', (req, res) => {
+  console.log('Received GET request to /sayHello');
+  res.json({ message: 'Hello User' });
+  console.log('Sent response to /sayHello');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log('Server started successfully');
+});
